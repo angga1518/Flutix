eada32c609c3683fdc3f42721f2785e2cbae3abc
muhammad.erlangga <muhammad.erlangga@pinhome.id>
2020-12-24 15:29:04 +0700

    feat: add theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,16 @@
+part of 'shared.dart';
+
+const double defaultMargin = 24;
+
+Color mainColor = Color(0x503E9D);
+Color accentColor1 = Color(0x2C1F63);
+Color accentColor2 = Color(0xFBD460);
+Color accentColor3 = Color(0xADADAD);
+
+TextStyle blackTextFont = GoogleFonts.raleway().copyWith(color: Colors.black, fontWeight: FontWeight.w500);
+TextStyle whiteTextFont = GoogleFonts.raleway().copyWith(color: Colors.white, fontWeight: FontWeight.w500);
+TextStyle purpleTextFont = GoogleFonts.raleway().copyWith(color: mainColor, fontWeight: FontWeight.w500);
+TextStyle greyTextFont = GoogleFonts.raleway().copyWith(color: accentColor3, fontWeight: FontWeight.w500);
+
+TextStyle whiteNumberFont = GoogleFonts.openSans().copyWith(color: Colors.white, fontWeight: FontWeight.w500);
+TextStyle yellowNumberFont = GoogleFonts.openSans().copyWith(color: accentColor2, fontWeight: FontWeight.w500);
