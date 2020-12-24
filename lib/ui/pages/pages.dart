8d7a7ddae88f69750072c45916740a6414d546f0
muhammad.erlangga <muhammad.erlangga@pinhome.id>
2020-12-24 23:01:49 +0700

    feat: finish splash screen

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutix/bloc/blocs.dart';
 import 'package:flutix/services/services.dart';
 import 'package:flutix/shared/shared.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -5,15 +5,66 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     PageBloc pageBloc = BlocProvider.of<PageBloc>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Splash Page"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            pageBloc.add(GoToLoginPage());
-          },
-          child: Text("Go to sign in page"),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 136,
+                decoration: BoxDecoration(
+                    image:
+                        DecorationImage(image: AssetImage("assets/logo.png"))),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 16, top: 70),
+                child: Text(
+                  "New Experience",
+                  style: blackTextFont.copyWith(fontSize: 20),
+                ),
+              ),
+              Text(
+                "Watch a new movie much\neasier than any before",
+                style: greyTextFont.copyWith(
+                    fontSize: 16, fontWeight: FontWeight.w300),
+                textAlign: TextAlign.center,
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 20, top: 70),
+                width: 250,
+                height: 45,
+                child: RaisedButton(
+                  child: Text(
+                    "Get Started",
+                    style: whiteTextFont.copyWith(fontSize: 16),
+                  ),
+                  color: mainColor,
+                  onPressed: () {},
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account?",
+                    style: greyTextFont.copyWith(
+                        fontSize: 14, fontWeight: FontWeight.w400),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      pageBloc.add(GoToLoginPage());
+                    },
+                    child: Text(
+                      " Sign In",
+                      style: purpleTextFont.copyWith(
+                          fontSize: 14, fontWeight: FontWeight.w500),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
