badd6abd55a8f2eebe9669776ee78f20521b4984
muhammad.erlangga <muhammad.erlangga@pinhome.id>
2020-12-26 11:39:49 +0700

    feat: homepage

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -6,8 +6,19 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int bottomNavigationBarIndex;
+  PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    bottomNavigationBarIndex = 0;
+    pageController = PageController(initialPage: bottomNavigationBarIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    UserBloc userBloc = BlocProvider.of<UserBloc>(context);
     return Scaffold(
         body: Stack(
       children: [
@@ -17,7 +28,20 @@ class _MainPageState extends State<MainPage> {
             color: Color(0xFFF6F7F9),
           ),
         ),
-        ListView(),
+        PageView(
+          controller: pageController,
+          onPageChanged: (index) {
+            setState(() {
+              bottomNavigationBarIndex = index;
+            });
+          },
+          children: <Widget>[
+            MoviePage(),
+            Center(
+              child: Text("My Tickets"),
+            )
+          ],
+        ),
         customBottomNavBar(),
         Align(
           alignment: Alignment.bottomCenter,
@@ -26,6 +50,11 @@ class _MainPageState extends State<MainPage> {
             width: 46,
             margin: EdgeInsets.only(bottom: 42),
             child: FloatingActionButton(
+                onPressed: () {
+                  userBloc.add(SignOut());
+                  AuthServices.signOut();
+                },
+                backgroundColor: accentColor2,
                 elevation: 0,
                 child: SizedBox(
                     height: 26,
@@ -39,22 +68,59 @@ class _MainPageState extends State<MainPage> {
       ],
     ));
   }
-}
 
-Widget customBottomNavBar() {
-  return Align(
-    alignment: Alignment.bottomCenter,
-    child: ClipPath(
-      clipper: BottomNavBarClipper(),
-      child: Container(
-        height: 70,
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+  Widget customBottomNavBar() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: ClipPath(
+        clipper: BottomNavBarClipper(),
+        child: Container(
+          height: 70,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+          child: BottomNavigationBar(
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            selectedItemColor: mainColor,
+            unselectedItemColor: Color(0xFFE5E5E5),
+            currentIndex: bottomNavigationBarIndex,
+            onTap: (index) {
+              setState(() {
+                bottomNavigationBarIndex = index;
+                pageController.jumpToPage(index);
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                  title: Text("New Movies",
+                      style: GoogleFonts.raleway(
+                          fontSize: 13, fontWeight: FontWeight.w600)),
+                  icon: Container(
+                    margin: EdgeInsets.only(bottom: 6),
+                    height: 20,
+                    child: Image.asset((bottomNavigationBarIndex == 0)
+                        ? "assets/ic_movies.png"
+                        : "assets/ic_movies_grey.png"),
+                  )),
+              BottomNavigationBarItem(
+                  title: Text("My Tickets",
+                      style: GoogleFonts.raleway(
+                          fontSize: 13, fontWeight: FontWeight.w600)),
+                  icon: Container(
+                    margin: EdgeInsets.only(bottom: 6),
+                    height: 20,
+                    child: Image.asset((bottomNavigationBarIndex == 1)
+                        ? "assets/ic_tickets.png"
+                        : "assets/ic_tickets_grey.png"),
+                  )),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
 
 class BottomNavBarClipper extends CustomClipper<Path> {
